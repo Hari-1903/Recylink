da94c98388a3bb3ebce6a53466d8f99356a078e2
Akhila <90416008+ry-aki@users.noreply.github.com>
2023-12-15 14:42:49 +0530

    Added Side NavBar

--- components/UserNav.tsx
+++ components/UserNav.tsx
@@ -10,35 +10,33 @@ import { MdOutlineExplore } from "react-icons/md";
 
 
 
-
-
 const UserNav = () => {
   return (
     <div className='bg-white shadow-[0px_0px_10px_10px_rgba(0,0,0,0.1)] w-full h-full rounded-lg py-5 flex flex-col justify-between items-start'>
-        <a href="/User" className='ml-7'>
+        <Link href="/User" className='ml-7'>
             <Image src="/assets/icons/recylink_black.png" width={100} height={100} alt="logo" />
-        </a>
+        </Link>
         <div className="flex flex-col items-start gap-2 h-full w-full">
             {/* <div className="flex flex-col gap-4"> */}
                 <h1 className='text-base text-left cursor-pointer font-bold border-b mt-5 ml-7 '>QUICK MENU</h1>
                 <div className='w-full'>
                     <div className='flex justify-start items-center w-full gap-4 pl-5 text-gray-400 hover:text-primary hover:shadow-lg p-3'>
-                    <MdOutlineAddToPhotos />
+                    <MdOutlineAddToPhotos size="1.15rem" />
                     <Link href='/User/RaiseTicket'>Complaint</Link>
                     </div>
 
                     <div className='flex justify-start items-center w-full gap-4 pl-5 text-gray-400 hover:text-primary hover:shadow-lg p-3 '>
-                    <FaRegFileAlt />
+                    <FaRegFileAlt size="1.15rem"/>
                     <Link href='/User/RaiseTicket'>Status</Link>
                     </div>
                     
                     <div className='flex justify-start items-center w-full gap-4 pl-5 text-gray-400 hover:text-primary hover:shadow-lg p-3 '>
-                    <LuCrown />
+                    <LuCrown size="1.15rem"/>
                     <Link href='/User/RaiseTicket'>Reward</Link>
                     </div>
 
                     <div className='flex justify-start items-center w-full gap-4 pl-5 text-gray-400 hover:text-primary hover:shadow-lg p-3 '>
-                    <MdOutlineExplore />
+                    <MdOutlineExplore size="1.15rem" />
                     <Link href='/User/RaiseTicket'>Explore</Link>
                     </div>
 
@@ -48,30 +46,27 @@ const UserNav = () => {
                 <h1 className='text-base text-left cursor-pointer font-bold border-b mt-5 ml-7 '>MORE</h1>
                 <div className='w-full'>
 
-                    
-                    
-
                     <div className='flex justify-start items-center w-full gap-4 pl-5 text-gray-400 hover:text-primary hover:shadow-lg p-3 '>
-                    <MdOutlineAddToPhotos />
+                    <MdOutlineAddToPhotos size="1.15rem"/>
                     <Link href='/User/RaiseTicket'>option1</Link>
                     </div>
                     
                     <div className='flex justify-start items-center w-full gap-4 pl-5 text-gray-400 hover:text-primary hover:shadow-lg p-3 '>
-                    <MdOutlineAddToPhotos />
+                    <MdOutlineAddToPhotos size="1.15rem"/>
                     <Link href='/User/RaiseTicket'>option2</Link>
                     </div>
 
                     <div className='flex justify-start items-center w-full gap-4 pl-5 text-gray-400 hover:text-primary hover:shadow-lg p-3 '>
-                    <MdOutlineAddToPhotos />
+                    <MdOutlineAddToPhotos size="1.15rem"/>
                     <Link href='/User/RaiseTicket'>option3</Link>
                     </div>
 
                     <div className='flex justify-start items-center w-full gap-4 pl-5 text-gray-400 hover:text-primary hover:shadow-lg p-3 '>
-                    <MdOutlineAddToPhotos />
+                    <MdOutlineAddToPhotos size="1.15rem"/>
                     <Link href="/Contact">Contact</Link>
                     </div>
 
-                    <div className='flex justify-center items-center gap-1 mt-3 mx-7 pl-5 text-white bg-primary hover:bg-dark-primary hover:shadow-lg p-2.5 rounded-ld'>
+                    <div className='flex justify-center items-center gap-1 mt-3 mx-7 pl-5 text-white bg-primary hover:bg-dark-primary hover:shadow-lg p-2.5 rounded-lg'>
                     <RiLogoutCircleFill size="1.25rem"/>
                     <Link href='/User'>Log Out</Link>
                     </div>
