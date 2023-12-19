64dee861f0873153d98e5b0712b97753ed03495e
NigamAnanya <as1450@srmist.edu.in>
2023-12-19 12:41:58 +0530

    Updated Type for UpdateStatus

--- pages/Authority/UpdateStatus/index.tsx
+++ pages/Authority/UpdateStatus/index.tsx
@@ -1,24 +1,46 @@
 import React, { useState } from 'react';
 import { Box, VStack, SimpleGrid, Text, Image, Heading, Badge } from '@chakra-ui/react';
-import AuthorityNav from '@/components/AuthorityNav';
-import { useImage } from '@/components/ImageContext';
+import AuthorityNav from '@/components/AuthorityNav'; // Assuming this is a custom component
+import { useImage } from '@/components/ImageContext';  // Assuming this is a custom hook
 
-const UpdateStatus = () => {
-  // Changed to use an object to track time for each complaint
-  const [completionTimes, setCompletionTimes] = useState({});
-  const { imageDetails } = useImage();
+// Define the type for the predictions in the API response
+type Prediction = {
+  class: string;
+};
 
-  // Update the completion time for a specific complaint
-  const handledone = (complaintNumber) => {
+// Define the type for the image detail
+type ImageDetail = {
+  complaintNumber: number;
+  location: string;
+  api_Response: {
+    predictions: Prediction[];
+  };
+  timeOfComplaint: string;
+  status: string;
+};
+
+// Assuming the useImage hook returns an object with an imageDetails array
+type ImageContextType = {
+  imageDetails: ImageDetail[];
+};
+
+const UpdateStatus: React.FC = () => {
+  const [completionTimes, setCompletionTimes] = useState<Record<number, string>>({});
+  const { imageDetails } = useImage() as ImageContextType; // Cast to the expected type
+
+  // Handle completion time update
+  const handledone = (complaintNumber: number): void => {
     setCompletionTimes(prevTimes => ({
       ...prevTimes,
       [complaintNumber]: new Date().toLocaleString()
     }));
-  }
+  };
 
   return (
     <div className="grid grid-cols-[280px_1fr] h-screen overflow-hidden">
-      <div className="bg-black my-3 ml-3 rounded-xl shadow-nav"><AuthorityNav/></div>
+      <div className="bg-black my-3 ml-3 rounded-xl shadow-nav">
+        <AuthorityNav />
+      </div>
       <div className="m-3 p-5 rounded-xl relative shadow-content flex flex-col">
         <div className='grid grid-cols-[0.75fr_0.25fr] grid-rows-[0.75fr_0.25fr] gap-6 h-full'>
           <table className="table-auto w-full">
@@ -36,12 +58,12 @@ const UpdateStatus = () => {
             <tbody>
               {imageDetails.map((detail, index) => (
                 <tr key={detail.complaintNumber} className="hover:bg-gray-50">
-                  <td className="border px-4 py-2 ">{index + 1}</td>
+                  <td className="border px-4 py-2">{index + 1}</td>
                   <td className="border px-4 py-2">{detail.location}</td>
                   <td className="border px-4 py-2">
-                    {detail.api_Response.predictions.map((pred, index) => (
-                      <React.Fragment key={index}>
-                        {pred.class}{index !== detail.api_Response.predictions.length - 1 ? ', ' : ''}
+                    {detail.api_Response.predictions.map((pred, idx) => (
+                      <React.Fragment key={idx}>
+                        {pred.class}{idx !== detail.api_Response.predictions.length - 1 ? ', ' : ''}
                       </React.Fragment>
                     ))}
                   </td>
@@ -52,14 +74,20 @@ const UpdateStatus = () => {
                     <div className='flex-center gap-1'>
                       <button className='text-s bg-green-500 p-2 rounded-3xl' onClick={() => handledone(detail.complaintNumber)}>Done</button>
                       <button className='text-s bg-red-500 p-2 rounded-3xl'>Spam</button>
-                    </div> 
+                    </div>
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
-          <div className='h-full rounded-md row-span-2 shadow-lg'>some graph</div>
-          <div className='h-full rounded-md shadow-lg'>Total details</div>
+          <div className='h-full rounded-md row-span-2 shadow-lg'>
+            {/* Placeholder for Graph Component */}
+            <Text>Some Graph</Text>
+          </div>
+          <div className='h-full rounded-md shadow-lg'>
+            {/* Placeholder for Total Details Component */}
+            <Text>Total Details </Text>
+          </div>
         </div>
       </div>
     </div>
