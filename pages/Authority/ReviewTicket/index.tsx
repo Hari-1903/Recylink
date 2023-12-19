9b133d54fb44306fa892324c38dac86da52a3a40
Hari-1903 <hnnair1903@gmail.com>
2023-12-19 16:56:29 +0530

    Feat : Mobile Responsiveness for Authority

--- pages/Authority/ReviewTicket/index.tsx
+++ pages/Authority/ReviewTicket/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { Box, VStack, SimpleGrid, Text, Image, Heading, Badge } from '@chakra-ui/react';
 import { useImage } from '@/components/ImageContext';
 import AuthorityNav from '@/components/AuthorityNav';
+import MobileNav from '@/components/MobileNav';
 
 const ReviewComplaints = () => {
   const { imageDetails } = useImage();
 
   return (
-    <div className="grid grid-cols-[280px_1fr] h-screen overflow-hidden">
-      <div className="bg-black my-3 ml-3 rounded-xl shadow-nav"><AuthorityNav/></div>
+    <div className="grid md:grid-cols-[280px_1fr] gr  id-cols-1 h-screen overflow-hidden">
+      <div className="bg-black my-3 ml-3 rounded-xl shadow-nav  hidden md:block" id='nav'><AuthorityNav/></div>
       <div className="m-3 px-5 py-3 rounded-xl relative shadow-content flex flex-col">
         <p className="text-xl font-bold border-b-2 mb-4 p-4">Raised Complaints</p>
-        <div className='h-[600px] grid grid-cols-5 gap-y-5 overflow-y-scroll px-5 pb-5 pt-2'>
+        <div className='md:h-[600px] grid grid-cols-5 gap-y-5 overflow-y-scroll px-5 pb-5 pt-2'>
           {imageDetails.map((detail: any) => (
             <div className="w-[200px] px-4 py-3 shadow-[0px_0px_6px_2px_rgb(0,0,0,0.06)] hover:bg-slate-100 hover:scale-110 transition-all 2s ease-in-out rounded-md" key="">
               <div className='flex justify-between items-center pb-1 border-b-2 mb-3'>
@@ -42,6 +43,7 @@ const ReviewComplaints = () => {
           ))}
         </div>
       </div>
+      <MobileNav/>
     </div>
 
 );
